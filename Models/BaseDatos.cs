ce6e7a8bcc4e5882185c6c9e0e770c8def9a9dda
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-07-04 15:57:23 -0300

    las bases

--- Models/BaseDatos.cs
+++ Models/BaseDatos.cs
@@ -1,2 +1,7 @@
 using Microsoft.Data.SqlClient;
 using Dapper;
+
+public static class BaseDatos
+{
+    private static string_connectionString = @"Server=localhost; DataBase Nombre Base; Integrated Security=True; TrustServerCertificate=True;";
+}
