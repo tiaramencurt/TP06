e521fff70f763112032f0661a77a95f2f4b3279c
Maica <maica.trutner@gmail.com>
2025-07-05 09:41:17 -0300

    usings de BD

--- Models/BaseDatos.cs
+++ Models/BaseDatos.cs
@@ -1,7 +1,11 @@
 using Microsoft.Data.SqlClient;
 using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
 
 public static class BaseDatos
 {
-    private static string_connectionString = @"Server=localhost; DataBase Nombre Base; Integrated Security=True; TrustServerCertificate=True;";
+    private static string_connectionString = @"Server=localhost; DataBase=Integrante; Integrated Security=True; TrustServer Certificate=True;";
 }
